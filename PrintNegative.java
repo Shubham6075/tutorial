3d22b077641a7c358a5409e85fb846bb22e9b409
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-02-26 19:52:20 +0530

    Add files via upload

---
+++ PrintNegative.java
@@ -0,0 +1,17 @@
+package array1;
+
+public class PrintNegative {
+	public static void main(String[] args) {
+		int [] arr = {10,20,-23,-34,-5,67,-1};
+		
+		for(int i =0;i<arr.length;i++) {
+			
+		if(arr[i]<0) {
+				System.out.println(arr[i]);	
+			}	
+		
+		}
+		
+	}
+
+}
