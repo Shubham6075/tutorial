52478f396606c53141c98e6bbe3e2533253d32f2
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-03-01 15:25:29 +0530

    Add files via upload

---
+++ RepeatedElement.java
@@ -0,0 +1,19 @@
+package array1;
+
+public class RepeatedElement {
+	public static void main(String[] args) {
+		int []my_array=new int[] {1,2,3,3,4,5,6,7,7,8,9};
+
+		for(int i=0;i<my_array.length;i++) {
+
+			for(int j=i+1;j<my_array.length;j++) {
+
+				if(my_array[i]==my_array[j]) {
+
+					System.out.println(my_array[j]);
+				}
+			}
+		}
+	}
+
+}
