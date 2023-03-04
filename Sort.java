75d49910eab30825975c33acd4272ef42c1412c7
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-03-04 08:45:39 +0530

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,20 @@
+package array1;
+
+import java.util.Arrays;
+
+public class Sort {
+	public static void main(String[] args) {
+		int[] b = {50,30,20,40,10};
+		
+	    System.out.println(Arrays.toString(b));
+		
+		System.out.println("*********");
+		
+		Arrays.sort(b);
+		
+		System.out.println(Arrays.toString(b));
+		
+		
+	}
+
+}
